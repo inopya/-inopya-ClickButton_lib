10207d692e1a3d3c0f7b8fbe05980b16e4a099a4
Eulogio Lopez <usuariogit@hotmail.com>
2020-03-27 21:29:29 +0100

    Update ejemplo_pulsador_multi_deteccion.ino

--- ejemplo_pulsador_multi_deteccion/ejemplo_pulsador_multi_deteccion.ino
+++ ejemplo_pulsador_multi_deteccion/ejemplo_pulsador_multi_deteccion.ino
@@ -46,7 +46,7 @@
 					Ejemplo de uso:
   *******************************************************
    >> solo con el numero de pin
-      ClickButton mi_pulsador_1(uint8_t pin); el resto de parametros por defecto: son (30,150,380)
+      ClickButton mi_pulsador_1(uint8_t pin); el resto de parametros por defecto: son (30,100,380)
    
    >> o bien definiendo todos los parametros disponibles
       ClickButton mi_pulsador_2(uint8_t pin, uint8_t antirrebotes, uint16_t corta, uint16_t larga);
